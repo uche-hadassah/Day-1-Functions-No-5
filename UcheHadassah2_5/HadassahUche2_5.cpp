195749e1bc71d8b2b9a49d8405284e8a1f24e319
Hadassah Uche <uhadassah1@gmail.com>
2023-11-01 15:56:36 +0200

    I added an interesting set of input validations and appropriate comments in the main(¬_¬")

--- UcheHadassah2_5/HadassahUche2_5.cpp
+++ UcheHadassah2_5/HadassahUche2_5.cpp
@@ -1,10 +1,6 @@
-/*Write a function that takes two characters ch1 and ch2 as parameters and prints all 
-characters between ch1 and ch2, with a specified number of characters per line. Use 
-the following function header:
-void printChars(char ch1, char ch2, int numberPerLine)
-For example printChars(‘d’, ‘z’, 10) will print 10 characters per line from ‘d’ to ‘z’
-(excluding ‘d’ and ‘z’), separated by exactly one space.
-(You may assume that ch1< ch2 in the ASCII)*/
+/*Name:Uche Hadassah
+This program prints a specific number of characters per line, from the first(ch1) to the last(ch2), 
+depending on the user input*/
 #include<iostream>
 using namespace std;
 void printChars(char ch1, char ch2, int numberPerLine);
@@ -18,7 +14,18 @@ int main()
 	cin >> Ch2;
 	cout << "Enter how many characters you want to display per line:";
 	cin >> numPerLine;
-	printChars(Ch1, Ch2, numPerLine);
+	if (Ch1 < Ch2)
+	{
+		printChars(Ch1, Ch2, numPerLine);
+	}
+	else if (Ch1 > Ch2)//Validation of input when the first character is bigger than the second character
+	{
+		cout << "Wrong order! I can't print from " << Ch1 << " to " << Ch2 << endl;
+	}
+	else// when ch1 = ch2
+	{
+		cout << Ch2 << endl;
+	}
 	return 0;
 }
 void printChars(char ch1, char ch2, int numberPerLine)
