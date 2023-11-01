3ad4b6eb9d7d791ce20a420025bc3c14bda6880c
Hadassah Uche <uhadassah1@gmail.com>
2023-11-01 16:03:04 +0200

    Cleaned up the code and added useful comments

--- UcheHadassah2_5/HadassahUche2_5.cpp
+++ UcheHadassah2_5/HadassahUche2_5.cpp
@@ -1,9 +1,10 @@
 /*Name:Uche Hadassah
 This program prints a specific number of characters per line, from the first(ch1) to the last(ch2), 
-depending on the user input*/
+depending on the user input. 
+Note: I have not included printing in reverse as the question did not say so.*/
 #include<iostream>
 using namespace std;
-void printChars(char ch1, char ch2, int numberPerLine);
+void printChars(char ch1, char ch2, int numberPerLine);//Function Prototype
 int main()
 {
 	char Ch1, Ch2;
@@ -16,13 +17,13 @@ int main()
 	cin >> numPerLine;
 	if (Ch1 < Ch2)
 	{
-		printChars(Ch1, Ch2, numPerLine);
+		printChars(Ch1, Ch2, numPerLine);//Calling the function
 	}
 	else if (Ch1 > Ch2)//Validation of input when the first character is bigger than the second character
 	{
 		cout << "Wrong order! I can't print from " << Ch1 << " to " << Ch2 << endl;
 	}
-	else// when ch1 = ch2
+	else//Validation of input when ch1 = ch2
 	{
 		cout << Ch2 << endl;
 	}
@@ -35,14 +36,14 @@ void printChars(char ch1, char ch2, int numberPerLine)
 	{
 		for (int i = 0; i < numberPerLine; i++)
 		{
-			if (character <= ch2)
+			if (character <= ch2)//Does this until we reach the second character
 			{
 				cout << character << " ";
-				character++;
+				character++;//Moves to the next character
 			}
-			else
+			else//Once we reach ch2 + 1
 				break;
 		}
 		cout << endl;
-	} while (character < ch2 +1);
+	} while (character < ch2 + 1);//Loops as long as we are within the range of ch1 to ch2 included
 }
